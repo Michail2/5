e2ddae531a33e60c30f1f13f3daca84a769cfb9c
our ame <you@example.com>
2022-10-17 17:51:19 +0300

    Задания к 5 уроку

---
+++ Задача 2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написно конечно на коленке и не совсем так как должно быть, он все же
+// По хрошему я должен написать написать условия при которых из координат возможно построить фигуру, 
+// условия при которых только Х или только Y должен умножаться на коэффициент или и X и Y вместе 
+Console.Write("Введите коэффициент маштабирования: ");
+var K = Convert.ToDouble(Console.ReadLine());
+double[,] table1 = new double[2, 4];
+double[,] table = new double[2, 4];
+for(int i = 0; i < 2; ++i) 
+        {
+            for(int j=0; j < 4; ++j) 
+            {   
+                if ( i == 0)
+                {
+                    Console.Write($"Ведите X{j}: ");
+                    table[i,j] = Convert.ToDouble(Console.ReadLine());
+                }            
+                else if ( i == 1)
+                {
+                  Console.Write($"Ведите Y{j}: "); 
+                  table[i,j] = Convert.ToDouble(Console.ReadLine()); 
+                }
+                table1[i,j] = table[i,j] * K;   
+            }                      
+        }
+ 
+        Console.WriteLine("Координаты исходных вершин: ");
+        Console.WriteLine($"({table[0 , 0]}, {table[1 , 0]}) ({table[0 , 1]}, {table[1 , 1]}) ({table[0 , 2]}, {table[1 , 2]}) ({table[0 , 3]}, {table[1 , 3]})"); 
+        Console.WriteLine($"При K = {K} координаты вершин в маштабе: ");
+        Console.WriteLine($"({table1[0 , 0]}, {table1[1 , 0]}) ({table1[0 , 1]}, {table1[1 , 1]}) ({table1[0 , 2]}, {table1[1 , 2]}) ({table1[0 , 3]}, {table1[1 , 3]})");
+
+
---
+++ Задача 3/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine()); // Числовой диапазон
+int[] arr = new int [a];
+ for( int i = 0; i < a; i++)
+{
+ arr[i] = new Random().Next(1, m);
+ Console.Write(arr[i] + " ");
+}
+Console.WriteLine(" ");
+
+        int[] copy = new int[a];
+        arr.CopyTo(copy, 0);
+        Console.WriteLine("Вывод скопированного массива: ");
+        Console.WriteLine(String.Join(" ", copy));
